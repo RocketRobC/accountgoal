33fb59e1da12a908aa0773c680df5b33993bfe72
Rob Cornish <rob@robcornishmusic.com>
2015-09-21 23:30:03 +1000

    admin_pass profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  # prepend_before_action :admin_pass, only: [:index]
+  prepend_before_action :admin_pass, only: [:index]
   prepend_before_action :set_profile, except: [:index]
   skip_before_action :profile_check
 
